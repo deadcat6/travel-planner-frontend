4fc2b8881cb2290c56e3038926508c92d8cda7b7
songhany <ysh961008@yeah.net>
2022-06-22 22:59:33 -0400

    update

--- src/ui/plan/CreatePlaceOption.tsx
+++ src/ui/plan/CreatePlaceOption.tsx
@@ -7,31 +7,38 @@ import { DateRangePicker } from '@mantine/dates';
 import { Title, Box, Checkbox, Image, Button, Group, MultiSelect, Text,Textarea } from '@mantine/core';
 import { placeType } from "./PlanView";
 
-export const CreatePlaceOption = ({selectedPlace}) => {
-    const [place, setPlace] = useState<placeType>({
-            id: "",
-            note: "",
-            placeDuration: {
-                startTime: new Date(),
-                endTime: new Date(),
-            },
-            //iris
-            type: "",
-            title: "",
-            image: "",
-            geo: {
-                lat: 0,
-                lng: 0,
-            },
-            rating: 0, // google api
-            popularity: 0, //counter
-        }
-    );
+export const CreatePlaceOption = ({selectedPlace, setPlace, submitHandler}) => {
+    // const [place, setPlace] = useState<placeType>({
+    //         id: "",
+    //         note: "",
+    //         placeDuration: {
+    //             startTime: new Date(),
+    //             endTime: new Date(),
+    //         },
+    //         //iris
+    //         type: "",
+    //         title: "",
+    //         image: "",
+    //         geo: {
+    //             lat: 0,
+    //             lng: 0,
+    //         },
+    //         rating: 0, // google api
+    //         popularity: 0, //counter
+    //     }
+    // );
     const [property, setProperty] = useState(['Hotel', 'Museum', 'Garden', 'Hot Spring'])
     const [value, setValue] = useState<[Date | null, Date | null]>([
         new Date(2022, 0, 1),
         new Date(2022, 0, 5),
     ]);
+    // const formInfo = {
+    //     id: '',
+    //     title: '',
+    //     image: '',
+    //     dataRange: '',
+    //     placeProperty: '',
+    //   }
 
     const form = useForm({
         initialValues: {
@@ -40,16 +47,35 @@ export const CreatePlaceOption = ({selectedPlace}) => {
           image: '',
           dataRange: '',
           placeProperty: '',
-          save: false,
         },
     });
+
+    const clickSubmit = () => {
+        setPlace(
+            ...selectedPlace,
+            id: form.values.id,
+        )
+        submitHandler();
+    }
+
 
     return (
         <Box className='create'>
             <Title order={2}>Submit a Place Option</Title>
-            <form onSubmit={form.onSubmit((values) => console.log(values))}>
+            <form 
+                onSubmit={
+                    form.onSubmit((values) => {
+                        console.log(values);
+                        console.log(form.values.id);
+
+                        console.log(selectedPlace)
+
+                    })
+
+                }
+            >
                 <Textarea
-                    placeholder={uuidv4()}
+                    placeholder={selectedPlace.id}
                     label="Id"
                     required
                     {...form.getInputProps('id')}
@@ -91,15 +117,8 @@ export const CreatePlaceOption = ({selectedPlace}) => {
                     {...form.getInputProps('placeProperty')}
                 />
 
-                {/* const clickSubmit = () => {
-                    setPlace(
-                        .....
-                    )
-                    submitHandler()
-                } */}
-
                 <Group position="right" mt="md">
-                    <Button type="submit">Add Place to List</Button>  
+                    <Button type="submit" onClick={clickSubmit}>Add Place to List</Button>  
                 </Group>
             </form>
         </Box>
